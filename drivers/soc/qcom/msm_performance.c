748c4204b05fb141c5a924f84ce04fd1694f0465
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-08 16:38:36 +0000

    msm: performance: prevent the driver from handling userspace min freq cpu boost hints. We'll use the standard cpu-boost input-boost functionality which gives users freedom to choose their own settings
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -425,6 +425,7 @@ device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
  */
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -488,6 +489,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }

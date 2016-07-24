c3ec0b6e9f92f1bab668bda809d9472dbd9d3802
tbalden <illespal@gmail.com>
2016-07-24 04:53:58 -0700

    leds: qpnp rgb: Fading LED / Gradient charging LED / RGB led light level
    - adding RGB led light level for blinking and charging 0-20, 0 brightest
      It has impact on charge led light brightness and notif blinking as well.
      ( ./bln_rgb_blink_light_level)
    
    - adding Fading LED (pulsation) to Blinking Green LED workings
      instead of a 2 step LUT use a fade 7 step LUT and appropriate flags
      Take care of screen on/off blank event retriggering (that ruins
      fade work otherwise, so a restart is needed)
      ( ./bln_rgb_pulse)
    
    - adding Gradient charging LED based on charge level the constant
      color is tuned as a composition of amber and green. The more
      charged the battery is the greener it gets. At full battery it
      will start slowly blink between full green and yellowish color
      for easy notice of full charge.
      With sysfs to switch it on/off ( ./bln_rgb_batt_colored)
    
    - adding report_charge_level sympbol, and calling it from HTC battery
    
    Original idea of Gradient charging LED, original implementation
    and copyright by Pal Zoltan Illes aka @tbalden - July 2016

--- drivers/power/htc_battery.c
+++ drivers/power/htc_battery.c
@@ -504,6 +504,10 @@ static void adjust_store_level(int *store_level, int drop_raw, int drop_ui, int 
 	*store_level = store;
 }
 
+#if 1
+extern void register_charge_level(int level);
+#endif
+
 #define DISCHG_UPDATE_PERIOD_MS			(1000 * 60)
 #define ONE_PERCENT_LIMIT_PERIOD_MS		(1000 * (60 + 10))
 #define FIVE_PERCENT_LIMIT_PERIOD_MS	(1000 * (300 + 10))
@@ -1884,6 +1888,9 @@ static void batt_worker(struct work_struct *work)
 	batt_set_check_timer(htc_batt_timer.time_out);
 
 	/* STEP 3: update charging_source */
+#if 1
+	register_charge_level(htc_batt_info.rep.level);
+#endif
 	htc_batt_info.prev.charging_source = htc_batt_info.rep.charging_source;
 	htc_batt_info.rep.charging_source = g_latest_chg_src;
 

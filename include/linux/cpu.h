456a4187fdcfaee32ffe6b2bb7fd02181bf66b02
ninez <johnstonljordan@gmail.com>
2017-10-20 12:31:19 -0400

    Backport RCU from linux-4.4 [Local commit due to patching via git being a pain. lol]
    (cherry picked from commit b30a305cbfd8e917d9d95ce0c6f6b98dd1f1942d)

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -92,6 +92,8 @@ enum {
 					* Called on the new cpu, just before
 					* enabling interrupts. Must not sleep,
 					* must not fail */
+#define CPU_DYING_IDLE		0x000B /* CPU (unsigned)v dying, reached
+					* idle loop. */
 #define CPU_BROKEN		0x000C /* CPU (unsigned)v did not die properly,
 					* perhaps due to preemption. */
 
@@ -295,5 +297,15 @@ bool cpu_report_death(void);
 void idle_notifier_register(struct notifier_block *n);
 void idle_notifier_unregister(struct notifier_block *n);
 void idle_notifier_call_chain(unsigned long val);
+
+DECLARE_PER_CPU(bool, cpu_dead_idle);
+
+int cpu_report_state(int cpu);
+int cpu_check_up_prepare(int cpu);
+void cpu_set_state_online(int cpu);
+#ifdef CONFIG_HOTPLUG_CPU
+bool cpu_wait_death(unsigned int cpu, int seconds);
+bool cpu_report_death(void);
+#endif /* #ifdef CONFIG_HOTPLUG_CPU */
 
 #endif /* _LINUX_CPU_H_ */

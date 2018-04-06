3d169b498482cfb68aa68f964ea5e42abc9cf0cb
fagyi <fagyipda@gmail.com>
2018-04-06 12:34:27 +0200

    pme: disable htc pnp manager - Also fix the issue after that for compile

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -36,8 +36,6 @@ static int cfq_group_idle = HZ / 125;
 static const int cfq_target_latency = HZ * 3/10; /* 300 ms */
 static const int cfq_hist_divisor = 4;
 
-extern int launch_event_enabled;
-
 /*
  * offset from end of service tree
  */
@@ -3458,20 +3456,6 @@ static void cfq_init_prio_data(struct cfq_queue *cfqq, struct cfq_io_cq *cic)
 {
 	struct task_struct *tsk = current;
 	int ioprio_class;
-
-	if (launch_event_enabled)
-	{
-		int bg;
-		bg = (tsk->sched_task_group->shares < DEFAULT_CPU_SHARE)? 1 : 0;
-		if (bg) {
-			cfqq->ioprio_class = IOPRIO_CLASS_IDLE;
-			cfqq->ioprio = 7;
-			cfqq->org_ioprio = cfqq->ioprio;
-			cfq_clear_cfqq_idle_window(cfqq);
-			cfq_mark_cfqq_prio_changed(cfqq);
-			return;
-		}
-	}
 
 	if (!cfq_cfqq_prio_changed(cfqq))
 		return;

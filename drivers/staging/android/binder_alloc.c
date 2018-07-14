aee76804f3906b1e392362f0c0cd925216c8d0c7
fagyi <fagyipda@gmail.com>
2018-07-14 16:54:53 +0200

    pme: add back mmput_async - partially reverted this commit: https://github.com/Fagyi/android_kernel_htc_msm8996/commit/063b1113e5a6bdb03112a661a41e9152f56b09f6

--- drivers/staging/android/binder_alloc.c
+++ drivers/staging/android/binder_alloc.c
@@ -964,7 +964,7 @@ enum lru_status binder_alloc_free_page(struct list_head *item,
 	return LRU_REMOVED_RETRY;
 
 err_down_write_mmap_sem_failed:
-	mmput(mm);
+	mmput_async(mm);
 err_mmget:
 err_page_already_freed:
 	mutex_unlock(&alloc->mutex);

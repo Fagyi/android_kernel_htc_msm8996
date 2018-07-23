bc43587aae0a8ab504f75e7773fe3a4cc4833fda
fagyi <fagyipda@gmail.com>
2018-07-23 22:36:17 +0200

    pme: touchboost: derp fixes

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -26,9 +26,9 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 
-#ifndef CONFIG_MSM_PERFORMANCE_CPUFREQ_LIMITS_VOTING_ONLY
-
 static int touchboost = 0;
+
+#ifndef CONFIG_MSM_PERFORMANCE_CPUFREQ_LIMITS_VOTING_ONLY
 
 static unsigned int use_input_evts_with_hi_slvt_detect;
 static struct mutex managed_cpus_lock;
